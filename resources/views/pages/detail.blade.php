26816421ec5c5814c189bdfc37abfa8387ab0066
duckhoi-UET <72605244+duckhoi-UET@users.noreply.github.com>
2020-12-15 15:02:52 +0700

    update detail

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -4,9 +4,6 @@
 @endsection
 @section('content')
 <div id="main-body">
-                <div id="main-images">
-                </div>
-                    
                 <div class="container">
                     <div id="main-content" class="main" style="position: relative;">
                         <div id="center">
@@ -15,47 +12,45 @@
                                     <div class="moduletitle">
                                         <div class="blog-title">
                                             <div class="title">
-                                                <span class="type">{{$post->category->name}}</span>
-                                                <h3>{{$post->name}}</h3>
-                                                <a href="#" class="province">{{$post->convince}}</a>
+                                                <span class="type">Cho thuê trọ</span>
+                                                <h3>Phòng trọ trong nhà nguyên căn giờ giấc tự do có thang máy vào ở ngay hẻm 101 Gò Dầu Quận Tân Phú</h3>
+                                                <a href="#" class="province">Hồ Chí Minh</a>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="modulecontent">
                                         <div class="box-content">
-                                            <div class="line"></div>
-                                            <h2>Hình ảnh</h2>
-                                            <div class="images-room">
-                                                <ul>
-                                                    <li><img src="image/{{$post->image}}"></li>
-                                                </ul>
-                                            </div>
                                             <div class="info-wrapper">
-                                                <dl><dt>Địa chỉ:</dt><dd> {{$post->address}}</dd></dl>
-                                                <dl><dt>Giá:</dt><dd>Khoảng {{$post->price}} đồng/tháng</dd></dl>
+                                                <dl><dt>Địa chỉ:</dt><dd> Nguyễn Háo Vĩnh, Tân Quý, Tân Phú</dd></dl>
+                                                <dl><dt>Giá:</dt><dd>Khoảng 3.000.000 đồng/tháng</dd></dl>
                                                 <div class="info">
                                                     <div class="i-left">
-                                                        <dl><dt>Hình thức:</dt><dd>{{$post->category->name}}</dd></dl>
-                                                        <dl><dt>Diện tích:</dt><dd>Khoảng {{$post->area}} m2</dd></dl>
-                                                        <dl><dt>Số phòng:</dt><dd>{{$post->bedRoom}}</dd></dl>
-                                                        
+                                                        <dl><dt>Hình thức:</dt><dd>Dãy trọ - Chủ quản</dd></dl>
+                                                        <dl><dt>Diện tích:</dt><dd>Khoảng 25 m2</dd></dl>
+                                                        <dl><dt>Số phòng:</dt><dd>-</dd></dl>
+                                                        <dl><dt>Phòng trống:</dt><dd>3 phòng</dd></dl>
+                                                        <dl><dt>Ở tối đa:</dt><dd></dd></dl>
                                                     </div>
                                                     <div class="i-right">
                                                         <dl>
                                                             <dt>Người đăng:</dt>
-                                                            <dd><span>{{$post->user->name}}</span></dd>
+                                                            <dd><span>Đức Khôi</span></dd>
                                                         </dl>
                                                         <dl>
                                                             <dt>Điện thoại:</dt>
-                                                            <dd><span>{{$post->user->phone}}</span></dd>
+                                                            <dd><span>0962462692</span></dd>
+                                                        </dl>
+                                                        <dl>
+                                                            <dt>Facebook:</dt>
+                                                            <dd></dd>
                                                         </dl>
                                                         <dl>
                                                             <dt>Email:</dt>
-                                                            <dd>{{$post->user->email}}</dd>
+                                                            <dd></dd>
                                                         </dl>
                                                         <dl>
                                                             <dt>Ngày cập nhật:</dt>
-                                                            <dd class="published">{{$post->updated_at}}</dd>
+                                                            <dd class="published">10-12-2020 16:08:34</dd>
                                                         </dl>
                                                     </div>
                                                 </div>
@@ -63,7 +58,7 @@
                                             <div class="line"></div>
                                             <h2>Thông tin</h2>
                                             <div class="content-detail">
-                                                <div class="content"><p>{{$post->description}}</p>
+                                                <div class="content"><p>CHO THUÊ PHÒNG CAO CẤP TRONG NHÀ NGUYÊN CĂN THANG MÁY MỚI ĐẸP – HẺM 101 GÒ DẦU, QUẬN TÂN PHÚ.Phòng 1:- Diện tích: 25m2- Giá thuê: 3 triệu/tháng. - Đặc điểm: có ban công thoáng mát, phòng có máy lạnh, quạt, tolet riêng 2 phòng.Phòng 2:- Diện tích: 30m2- Giá thuê: 3.5 triệu/tháng. - Đặc điểm: có cửa sổ thoáng mát, có nội thất tương đối đầy đủ, tolet riêng.* TIỆN ÍCH: - Nhà mới nguyên căn, an ninh tốt, thoáng mát, sạch sẽ.- Chỗ để xe miễn phí, rộng rãi.- Lối đi riêng, có thang máy.- Giờ giấc đi lại tự do. Có máy giặt free trên sân thượng.- Camera 24/24, cửa thẻ từ ra vào đảm bảo an ninh.- Nước máy 80k/người. Wifi cực mạnh 50k/phòng. Rác và vệ sinh 50k/phòng.* Liên hệ C.Thư 0909 768 394 để được xem phòng</p>
                                                 </div>
                                             </div>
                                             <div class="line"></div>
@@ -84,20 +79,34 @@
                                                     <li><i class="ico icon"></i><span title="Camera an ninh">Camera an ninh</span></li>
                                                 </ul>
                                             </div>
+                                            <div class="line"></div>
+                                            <h2>Hình ảnh</h2>
+                                            <div class="images-room">
+                                                <ul>
+                                                    <li><img src="https://static123.com/phongtro123/uploads/images/thumbs/900x600/fit/2020/08/28/z2009490064891-26dfef1b0d81e238a2176eb4bf1a0822_1598581201.jpg"></li>
+                                                </ul>
+                                            </div>
+                                            <div class="line"></div>
+                                            <h2>Liên hệ Chính chủ</h2>
+                                            <div class="box-info-bottom">
+                                                <dl>
+                                                    <dt><i class="fa fa-user"></i></dt>
+                                                    <dd><h3>Đức Khôi</h3></dd>
+                                                </dl>
+                                                <dl>
+                                                <dt><i class="fa fa-phone"></i></dt>
+                                                <dd><h3>0962462692</h3></dd>
+                                                </dl>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            
                             <section class="comment-wrapper clearfix kcblist-inited">
-                                @if(isset($user))
-                                <form action = "comment/{{$post->id}}" role = "form">
-                                <div class = "form-group">
-                                    <textarea class = "" rows = "3" name = "content" method = "post"></textarea>
-                                    <input type="hidden" name = "_token" value = "{{csrf_token()}}"/>
+                                <div data-view="formcomment" class="form-comment clearfix kcbform-inited">
+                                    <textarea class="input-type-text comment_text_readonly" placeholder="Viết bình luận của bạn" data-input="content"></textarea>
+                                    <a href="" title="Gửi bình luận" class="send-comment" data-act="send"><b>Gửi bình luận</b></a>
                                 </div>
-                                <button type = "submit" class = "btn btn-primary">Gửi</button>
-                                @endif
                                 <div class="comment_list">
                                     <span class="box-head">Bình luận (0)</span>
                                     <div class="lst-comment" id="listComment">
@@ -121,11 +130,11 @@
                                     <div class="contacts">
                                         <dl>
                                             <dt><i class="fa fa-user"></i></dt>
-                                            <dd><h3>{{$post->user->name}}</h3></dd>
+                                            <dd><h3>Đức Khôi</h3></dd>
                                         </dl>
                                         <dl>
                                             <dt><i class="fa fa-mobile"></i></dt>
-                                            <dd><h3>{{$post->user->phone}}</h3></dd>
+                                            <dd><h3>0962462692</h3></dd>
                                         </dl>
                                     </div>
                                 </div>
@@ -138,15 +147,55 @@
                                     <h2 class="box-title">Liên quan</h2>
                                 </div>
                                 <div class="box-body">
-                                    @foreach($post_relate as $row)
                                     <div class="item">
-                                        <span class="local"><i class="fa fa-map-marker"></i><a href="detail/{{$row->id}}">{{$row->convince}}</a></span>
-                                        <a href="detail/{{$row->id}}" target="_blank">{{$row->name}}</a>
+                                        <span class="local"><i class="fa fa-map-marker"></i><a href="#">Hồ Chí Minh</a></span>
+                                        <a href="#" target="_blank">Phòng Trọ Quận 4 2tr5</a>
                                         <div class="price">
-                                            <span>{{$row->price}} đồng</span><i>/tháng</i>
+                                            <span>2.5 triệu</span><i>/tháng</i>
                                         </div>
                                     </div>
-                                    @endforeach
+                                    <div class="item">
+                                        <span class="local"><i class="fa fa-map-marker"></i><a href="#">Hồ Chí Minh</a></span>
+                                        <a href="#" target="_blank">Phòng Trọ Quận 4 2tr5</a>
+                                        <div class="price">
+                                            <span>2.5 triệu</span><i>/tháng</i>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <span class="local"><i class="fa fa-map-marker"></i><a href="#">Hồ Chí Minh</a></span>
+                                        <a href="#" target="_blank">Phòng Trọ Quận 4 2tr5</a>
+                                        <div class="price">
+                                            <span>2.5 triệu</span><i>/tháng</i>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <span class="local"><i class="fa fa-map-marker"></i><a href="#">Hồ Chí Minh</a></span>
+                                        <a href="#" target="_blank">Phòng Trọ Quận 4 2tr5</a>
+                                        <div class="price">
+                                            <span>2.5 triệu</span><i>/tháng</i>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <span class="local"><i class="fa fa-map-marker"></i><a href="#">Hồ Chí Minh</a></span>
+                                        <a href="#" target="_blank">Phòng Trọ Quận 4 2tr5</a>
+                                        <div class="price">
+                                            <span>2.5 triệu</span><i>/tháng</i>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <span class="local"><i class="fa fa-map-marker"></i><a href="#">Hồ Chí Minh</a></span>
+                                        <a href="#" target="_blank">Phòng Trọ Quận 4 2tr5</a>
+                                        <div class="price">
+                                            <span>2.5 triệu</span><i>/tháng</i>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <span class="local"><i class="fa fa-map-marker"></i><a href="#">Hồ Chí Minh</a></span>
+                                        <a href="#" target="_blank">Phòng Trọ Quận 4 2tr5</a>
+                                        <div class="price">
+                                            <span>2.5 triệu</span><i>/tháng</i>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
