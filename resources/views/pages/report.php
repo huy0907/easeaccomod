75ee8438d35ca76699df09ca8cb654f62af64e88
dangsieuhoang <73376601+dangsieuhoang@users.noreply.github.com>
2020-12-20 11:06:07 +0700

    Create report.php

---
+++ resources/views/pages/report.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="css/dangtin.css">
+<script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+
+<h1 id="title">Báo Cáo</h1>
+
+<p id="description">Đề nghị các bạn cung cấp các nội dung chính xác, việc báo cáo chính xác giúp cho công động tốt hơn  </p>
+<p></p>
+
+<div id="body">
+
+<form id="survey-form">
+
+ <div class="loaiphong">
+  <p style="font-size: 30px; text-align: center;">Nội Dung Báo Cáo</p>
+  
+  <input type="radio" name="movie" value="1">Hình ảnh sai</input>
+  <br>
+  <input type="radio" name="movie" value="2">Giá quá cao</input>
+  <br>
+  <input type="radio" name="movie" value="3">Nhà ở chất lượng kém</input>
+  <br>
+  <input type="radio" name="movie" value="4">Hàng xóm ý thức kém</input>
+  <br>
+  <input type="radio" name="movie" value="5">Hay mất nước,mất điện</input>
+  <br>
+  <input type="radio" name="movie" value="6">Khu vực nhiều tệ nạn</input>
+  <br>
+  <br><br>
+
+  <div style="text-align: center;">Lý do khác</div>
+  <br><br>
+<textarea id="comment" rows="4" cols="40" placeholder="Nhập các lý do khác tại đây"></textarea>
+<br>
+<input id="submit" type="submit" value="Submit">
+            
+</form>
+
+</div>
+
+  
