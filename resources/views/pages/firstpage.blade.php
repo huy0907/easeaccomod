82633a8066038a204abb0fa9590979cc2cc118ab
duckhoi-UET <72605244+duckhoi-UET@users.noreply.github.com>
2020-12-20 11:37:08 +0700

    add css header

--- resources/views/pages/firstpage.blade.php
+++ resources/views/pages/firstpage.blade.php
@@ -177,6 +177,51 @@
             <div class="ads"></div>
             <!--NEWS-->
             <!--NEWS-->
+            <div class="news">
+                <div class="container">
+                <div class="news-header">
+                    <h2 class="header-title">Tin mới nhất</h2>
+                </div>
+                <div class="news-content">
+                    <div class="item">
+                        <div class="image">
+                            <a href="#"><img src="https://tromoi.com/uploads/members/hiephoang/thang%208/27_08/PHONG%20TH%E1%BB%A6Y%20PHONG%20TR%E1%BB%8C%20(2).png"></a>
+                        </div>
+                        <div class="content">
+                            <h4 class="title"><a href="#">Thuê nhà trọ: Bạn cần lưu ý những điều gì?</a></h4>
+                            <span class="published">22-10-2020 15:31:33</span>
+                            <div class="description">Thuê nhà  trọ tại các thành phố lớn là hoạt động vẫn luôn giữ được sự sôi động nhờ số lượng người dân sinh sống và làm việc đông đúc. Thời gian gần đây, dù ảnh hưởng của dịch bệnh Covid-19 nhưng nhu cầu tìm thuê nhà tại các tỉnh thành lớn vẫn tăng cao. Tuy nhiên, nếu là người đi thuê nhà trong thời điểm này, bạn cần phải lưu ý để tránh “tiền mất tật mang”. Cùng tromoi.com tìm hiểu về những lưu ý khi tìm thuê nhà nhé!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="image">
+                            <a href="#"><img src="https://tromoi.com/uploads/members/hiephoang/Thang%209/PHONG%20TH%E1%BB%A6Y%20PHONG%20TR%E1%BB%8C(1).png"></a>
+                        </div>
+                        <div class="content">
+                            <h4 class="title"><a href="#">Thuê phòng trọ với 5 bí kíp đơn giản nhất!</a></h4>
+                            <span class="published">22-10-2020 15:31:33</span>
+                            <div class="description">Phòng trọ tại các thành phố lớn luôn được rất nhiều khách hàng tìm kiếm. Tuy nhiên, để tìm thuê phòng trọ tại các thành phố lớn thì bạn cần lưu ý những gì? Hãy tham khảo ngay 5 bí kíp đơn giản nhất được chia sẻ dưới đây để tìm phòng nhanh nhất!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="image">
+                            <a href="#"><img src="https://tromoi.com/uploads/members/hiephoang/thang%208/27_08/PHONG%20TH%E1%BB%A6Y%20PHONG%20TR%E1%BB%8C%20(2).png"></a>
+                        </div>
+                        <div class="content">
+                            <h4 class="title"><a href="#">Thuê nhà trọ: Bạn cần lưu ý những điều gì?</a></h4>
+                            <span class="published">22-10-2020 15:31:33</span>
+                            <div class="description">Thuê nhà  trọ tại các thành phố lớn là hoạt động vẫn luôn giữ được sự sôi động nhờ số lượng người dân sinh sống và làm việc đông đúc. Thời gian gần đây, dù ảnh hưởng của dịch bệnh Covid-19 nhưng nhu cầu tìm thuê nhà tại các tỉnh thành lớn vẫn tăng cao. Tuy nhiên, nếu là người đi thuê nhà trong thời điểm này, bạn cần phải lưu ý để tránh “tiền mất tật mang”. Cùng tromoi.com tìm hiểu về những lưu ý khi tìm thuê nhà nhé!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="viewmore"><a href="https://tromoi.com/danh-sach">Xem thêm <i class="fa fa-angle-right"></i></a></div>
+                </div>
+                </div>
+            </div>
+            <!--NEWS-->
+            <!--NEWS-->
             <!--CONTACT-->
             <div class="box-contact">
                 <div class="container">
