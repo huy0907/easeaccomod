e340bc7b08ffed2040d95706990a15e290b36a05
Huy Doan <doanvanhuy0907@gmail.com>
2020-12-26 23:29:30 +0700

    fix post new

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -34,7 +34,9 @@
                                             <li> 
                                             <li class="drop_menu_child"><a href="notify">Thông báo</a></li>
                                             <li class="drop_menu_child"><a href="postlist">Quản lý tin</a></li>
+                                            @if($user->idRole !=2 )
                                             <li class="drop_menu_child"><a href="post">Đăng tin</a></li>
+                                            @endif
                                             <li class="drop_menu_child"><a href="savepost">Tin đã lưu</a></li>
                                             <hr>
                                             <li class="drop_menu_child"><a href="#">Hướng dẫn sử dụng</a></li>     
